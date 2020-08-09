2df8d5a6e0596af4d14d35332e15d47f2fd399e3
Hitesh <gupta.hitesh786@gmail.com>
2020-08-10 00:44:23 +0530

    Updates

---
+++ precache-manifest.35989ba8816673d68de0efa7263a4e18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07394c1f10e8710e707b",
+    "url": "/me/static/css/main.1f2e6b41.chunk.css"
+  },
+  {
+    "revision": "07394c1f10e8710e707b",
+    "url": "/me/static/js/main.07394c1f.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ab3b3d8990137ad933daae07286b8a1d",
+    "url": "/me/index.html"
+  }
+];
