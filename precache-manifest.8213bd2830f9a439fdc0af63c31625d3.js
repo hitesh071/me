372083aeb59877dea64004abb285d28932990fdb
Hitesh <gupta.hitesh786@gmail.com>
2022-05-31 00:03:35 +0530

    Updates

---
+++ precache-manifest.8213bd2830f9a439fdc0af63c31625d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "994331834fd5ecdc0afc",
+    "url": "/me/static/css/main.eef83a3e.chunk.css"
+  },
+  {
+    "revision": "994331834fd5ecdc0afc",
+    "url": "/me/static/js/main.99433183.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c0b80b3639dcf644bee35c121efa736f",
+    "url": "/me/index.html"
+  }
+];
